f37830bf192aa2e60fc83dbe76997dcd99859563
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-27 14:24:07 +0100

    Question 3.3

--- src/baseEx3Loop/BaseEx3Loop.java
+++ src/baseEx3Loop/BaseEx3Loop.java
@@ -2,34 +2,55 @@ package baseEx3Loop;
 import java.util.Scanner;
 
 public class BaseEx3Loop {
-	
-	 public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Entrez un nombre entier : ");
+    public static void main(String[] args) {
+        int nombre = 0;
+        boolean saisieValide = false;
 
-	        // Vérifie si la saisie est bien un entier
-	        if (scanner.hasNextInt()) {
-	            int nombre = scanner.nextInt();
+        // --- Lecture à partir des arguments ---
+        if (args.length > 0) {
+            // On parcourt tous les arguments (au cas où il y en aurait plusieurs)
+            for (String arg : args) {
+                try {
+                    nombre = Integer.parseInt(arg);
+                    saisieValide = true;
+                    break; // On prend le premier entier valide et on sort
+                } catch (NumberFormatException e) {
+                    System.out.println("Erreur : l'argument \"" + arg + "\" n'est pas un entier !");
+                }
+            }
+        }
 
-	            if (nombre > 0) {
-	                System.out.println("Le nombre est positif.");
-	            } else if (nombre < 0) {
-	                System.out.println("Le nombre est négatif.");
-	            } else {
-	                System.out.println("Le nombre est nul.");
-	            }
+        // --- Si aucun argument valide, on demande à l'utilisateur ---
+        if (!saisieValide) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Entrez un nombre entier : ");
 
-	            if (nombre % 2 == 0) {
-	                System.out.println("Le nombre est pair.");
-	            } else {
-	                System.out.println("Le nombre est impair.");
-	            }
-	        } else {
-	            System.out.println("La valeur n'est pas un entier");
-	        }
+            if (scanner.hasNextInt()) {
+                nombre = scanner.nextInt();
+                saisieValide = true;
+            } else {
+                System.out.println("Erreur : la valeur saisie n'est pas un entier !");
+            }
 
-	        scanner.close();
-	    }
+            scanner.close();
+        }
 
+        // --- Affichage du résultat ---
+        if (saisieValide) {
+            if (nombre > 0) {
+                System.out.println("Le nombre est positif.");
+            } else if (nombre < 0) {
+                System.out.println("Le nombre est négatif.");
+            } else {
+                System.out.println("Le nombre est nul.");
+            }
+
+            if (nombre % 2 == 0) {
+                System.out.println("Le nombre est pair.");
+            } else {
+                System.out.println("Le nombre est impair.");
+            }
+        }
+    }
 }
